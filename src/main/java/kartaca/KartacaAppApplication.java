98da0272c22740ef6ab90449c7f3d3ecf2ccfa06
nthY <n_hat.yalcin@outlook.com>
2023-03-31 18:14:54 +0300

    init

---
+++ src/main/java/kartaca/KartacaAppApplication.java
@@ -0,0 +1,13 @@
+package kartaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartacaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KartacaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kartaca/KartacaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kartaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KartacaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
