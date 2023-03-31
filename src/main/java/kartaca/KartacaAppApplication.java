d298eb0094b31d124eed3610b5711024625a7d7c
nthY <n_hat.yalcin@outlook.com>
2023-03-31 22:44:27 +0300

    prepare redis and swagger

--- src/main/java/kartaca/KartacaAppApplication.java
+++ src/main/java/kartaca/KartacaAppApplication.java
@@ -2,7 +2,14 @@ package kartaca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class KartacaAppApplication {
 
@@ -10,4 +17,14 @@ public class KartacaAppApplication {
 		SpringApplication.run(KartacaAppApplication.class, args);
 	}
 
+
+	// add Swagger Docker Bean to the Spring App class
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 }
